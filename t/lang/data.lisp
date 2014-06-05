dba99842d59ba7d2d80c48c8cf28f74deec67719
Masayuki Takagi <kamonama@gmail.com>
2014-06-05 14:32:52 +0900

    add blank files for CL-CUDA.LANG.DATA package

---
+++ t/lang/data.lisp
@@ -0,0 +1,16 @@
+#|
+  This file is a part of cl-cuda project.
+  Copyright (c) 2012 Masayuki Takagi (kamonama@gmail.com)
+|#
+
+(in-package :cl-user)
+(defpackage cl-cuda-test.lang.data
+  (:use :cl :cl-test-more
+        :cl-cuda.lang.data))
+(in-package :cl-cuda-test.lang.data)
+
+(plan nil)
+
+
+
+(finalize)
