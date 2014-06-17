190bd293131ea3c8e1f1c209b7a0fadd9523989e
Masayuki Takagi <kamonama@gmail.com>
2014-06-17 11:58:15 +0900

    modify to refactor CL-CUDA.API.CONTEXT package

--- src/api/context.lisp
+++ src/api/context.lisp
@@ -8,59 +8,81 @@
   (:use :cl
         :cl-cuda.driver-api
         :cl-cuda.api.kernel-manager)
-  (:export :*cuda-context*
-           :init-cuda-context
-           :release-cuda-context
-           :with-cuda-context
-           :synchronize-context))
+  (:export ;; Initialize CUDA
+           :init-cuda
+           ;; CUDA device
+           :get-cuda-device
+           :device-compute-capability
+           ;; CUDA context
+           :create-cuda-context
+           :destory-cuda-context
+           :synchronize-context
+           ;; WITH-CUDA-CONTEXT macro
+           :*cuda-device*
+           :*cuda-context*
+           :with-cuda-context))
 (in-package :cl-cuda.api.context)
 
 
 ;;;
-;;; CUcontext
+;;; Initialize CUDA
 ;;;
 
-(defun create-cu-context (dev-id)
-  (let (device context)
-    ;; initialize CUDA
-    (cu-init 0)
-    ;; get CUdevice
-    (cffi:with-foreign-object (device-ptr 'cu-device)
-      (cu-device-get device-ptr dev-id)
-      (setf device (cffi:mem-ref device-ptr 'cu-device)))
-    ;; create CUcontext
-    (cffi:with-foreign-object (context-ptr 'cu-context)
-      (cu-ctx-create context-ptr 0 device)
-      (setf context (cffi:mem-ref context-ptr 'cu-context)))
-    context))
+(defun init-cuda ()
+  (cu-init 0))
 
-(defun destroy-cu-context (context)
-  (cu-ctx-destroy context))
 
-(defmacro with-cu-context ((dev-id) &body body)
-  (with-gensyms (context)
-    `(let ((,context (create-cu-context ,dev-id)))
-       (unwind-protect (progn ,@body)
-         (destroy-cu-context ,context)))))
+;;;
+;;; CUDA device
+;;;
+
+(defun get-cuda-device (dev-id)
+  (cffi:with-foreign-object (device-ptr 'cu-device)
+    (cu-device-get device-ptr dev-id)
+    (cffi:mem-ref device-ptr 'cu-device)))
+
+(defun device-compute-capability (device)
+  (cffi:with-foreign-objects ((major :int)
+                              (minor :int))
+    (cu-device-compute-capability major minor device)
+    (values (cffi:mem-ref major :int)
+            (cffi:mem-ref minor :int))))
 
 
 ;;;
 ;;; CUDA context
 ;;;
 
-(defvar *cuda-context*)
+(defun create-cuda-context (device)
+  (cffi:with-foreign-object (context-ptr 'cu-context)
+    (cu-ctx-create context-ptr 0 device)
+    (cffi:mem-ref context-ptr 'cu-context)))
 
-(defun init-cuda-context (dev-id)
-  (create-cu-context dev-id))
-
-(defun release-cuda-context (context)
-  (kernel-manager-unload *kernel-manager*)
-  (destroy-cu-context context))
-
-(defmacro with-cuda-context ((dev-id) &body body)
-  `(let ((*cuda-context* (init-cuda-context ,dev-id)))
-     (unwind-protect (progn ,@body)
-       (release-cuda-context *cuda-context*))))
+(defun destroy-cuda-context (context)
+  (cu-ctx-destroy context))
 
 (defun synchronize-context ()
   (cu-ctx-synchronize))
+
+
+;;;
+;;; WITH-CUDA-CONTEXT macro
+;;;
+
+(defvar *cuda-device*)
+
+(defvar *cuda-context*)
+
+(defmacro with-cuda-context ((dev-id) &body body)
+  `(progn
+     ;; initialize CUDA
+     (init-cuda)
+     (let* (;; get CUDA device
+            (*cuda-device* (get-cuda-device ,dev-id))
+            ;; create CUDA context
+            (*cuda-context* (create-cuda-context *cuda-device*)))
+       (unwind-protect (progn ,@body)
+         ;; unload kernel manager
+         (kernel-manager-unload *kernel-manager*)
+         ;; destroy CUDA context
+         (destroy-cuda-context *cuda-context*)))))
