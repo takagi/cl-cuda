abde2d7b82cfd5067172dfa3445bc2f58575a2ab
Masayuki Takagi <kamonama@gmail.com>
2016-03-03 10:29:21 +1000

    Add selector macros for CUDA vector types' CL counterparts.

--- t/lang/data.lisp
+++ t/lang/data.lisp
@@ -26,5 +26,47 @@
       :test #'float3-=
       "basic case 1")))
 
+(subtest "with-float3"
+
+  (with-float3 (x y z) (make-float3 0.0 1.0 2.0)
+    (is x 0.0)
+    (is y 1.0)
+    (is z 2.0)))
+
+
+;;
+;; Float4
+
+(subtest "with-float4"
+
+  (with-float4 (x y z w) (make-float4 0.0 1.0 2.0 3.0)
+    (is x 0.0)
+    (is y 1.0)
+    (is z 2.0)
+    (is w 3.0)))
+
+
+;;
+;; Double3
+
+(subtest "with-double3"
+
+  (with-double3 (x y z) (make-double3 0d0 1d0 2d0)
+    (is x 0d0)
+    (is y 1d0)
+    (is z 2d0)))
+
+
+;;
+;; Double4
+
+(subtest "with-double4"
+
+  (with-double4 (x y z w) (make-double4 0d0 1d0 2d0 3d0)
+    (is x 0d0)
+    (is y 1d0)
+    (is z 2d0)
+    (is w 3d0)))
+
 
 (finalize)
