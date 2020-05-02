a187c351890ae1a7dddd52672664a0f41f86507c
Gabor Melis <mega@retes.hu>
2020-05-02 11:56:14 +0100

    Silence package variance warnings on SBCL.

--- src/cl-cuda.lisp
+++ src/cl-cuda.lisp
@@ -4,8 +4,14 @@
 |#
 
 (in-package :cl-user)
-(defpackage cl-cuda
-  (:use :cl :cl-reexport))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (locally
+      (declare #+sbcl
+               (sb-ext:muffle-conditions sb-kernel::package-at-variance))
+    (handler-bind
+        (#+sbcl (sb-kernel::package-at-variance #'muffle-warning))
+      (defpackage cl-cuda
+        (:use :cl :cl-reexport)))))
 (in-package :cl-cuda)
 
 (reexport-from :cl-cuda.driver-api
--- src/lang/lang.lisp
+++ src/lang/lang.lisp
@@ -4,9 +4,16 @@
 |#
 
 (in-package :cl-user)
-(defpackage :cl-cuda.lang
-  (:use :cl
-        :cl-reexport))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (locally
+      (declare #+sbcl
+               (sb-ext:muffle-conditions sb-kernel::package-at-variance))
+    (handler-bind
+        (#+sbcl (sb-kernel::package-at-variance #'muffle-warning))
+      (defpackage :cl-cuda.lang
+        (:use :cl
+              :cl-reexport)))))
+
 (in-package :cl-cuda.lang)
 
 ;; reexport symbols of data structures cl-cuda provides
